a09a1695dd26e9592064d0ec9e4aadc7f752e5ab
SAXENA-PARAM <paramsaxena1314@gmail.com>
2026-02-11 01:18:47 +0530

    demo

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,18 @@
 import postgres from 'postgres'
 import dotenv from 'dotenv'
+import { neon } from '@neondatabase/serverless';
 
 dotenv.config({ path: './.env' })
 
 // Initialize the connection outside the handler for connection pooling
-const sql = postgres(process.env.DATABASE_URL, {
-  ssl: 'require',
-  max: 1, // ✅ Vercel serverless: 1 connection per instance
-  idle_timeout: 20, // Close idle connections after 20s
-  connect_timeout: 10, // Timeout after 10s if can't connect
-  prepare: false, // ✅ REQUIRED for transaction pooler
-})
+// const sql = postgres(process.env.DATABASE_URL, {
+//   ssl: 'require',
+//   max: 1, // ✅ Vercel serverless: 1 connection per instance
+//   idle_timeout: 20, // Close idle connections after 20s
+//   connect_timeout: 10, // Timeout after 10s if can't connect
+//   prepare: false, // ✅ REQUIRED for transaction pooler
+// })
+const sql = neon(process.env.DATABASE_URL);
 
 
 export { sql }
