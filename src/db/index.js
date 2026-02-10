0702a5e7de078fbdf4e25304dd7a5762c8417a79
SAXENA-PARAM <paramsaxena1314@gmail.com>
2026-02-10 23:34:22 +0530

    transaction pooler for Supabase

--- src/db/index.js
+++ src/db/index.js
@@ -5,10 +5,9 @@ dotenv.config({ path: './.env' })
 
 // Initialize the connection outside the handler for connection pooling
 const sql = postgres(process.env.DATABASE_URL, {
-  ssl: 'require',
-  prepare: false, // MANDATORY for the Supabase Pooler
-  idle_timeout: 20, // Closes connection after 20 seconds of inactivity
-  max_lifetime: 60 * 30, // Max connection age is 30 minutes
+  ssl: "require",
+  prepare: false, // ✅ REQUIRED for pooler
+  max: 1,
 })
 
 export { sql }
